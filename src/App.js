9f5a330b6fa8cb02998a554d8542d321bf2c0d0a
Eliezer <eliezerlincon537@hotmail.com>
2021-04-29 21:30:10 -0300

    map implementation done

--- src/App.js
+++ src/App.js
@@ -4,36 +4,51 @@ const App = {
 	Start() {
 		App.View();
 	},
+
 	async View() {
 		const data = await Requester.requestData();
-		const infoElement = document.querySelector('main .info');
-
+		const infoElement = document.querySelector('main .info .text-info');
 
 		const html = `
-    <div class="text-info">
-      <div><strong>IP Público:</strong><span>${data.ip_address}</span></div>
-      <div>
-        <strong>Provedor:</strong
-        ><span title=${data.connection.organization_name}>${data.connection.organization_name}</span>
-      </div>
-      <div><strong>Cidade:</strong><span>${data.city} / ${data.region_iso_code}</span></div>
-      <div><strong>País:</strong><span>${data.country} ${data.flag.emoji}</span></div>
-      <div><strong>Horário atual:</strong><span>${data.timezone.current_time}</span></div>
-      <div><strong>É uma VPN:</strong><span>${data.security.is_vpn ? 'Sim' : 'Não'}</span></div>
+    <div><strong>IP Público:</strong><span>${data.ip_address}</span></div>
+    <div>
+      <strong>Provedor:</strong
+      ><span title=${data.connection.organization_name}>${data.connection.organization_name}</span>
     </div>
-    
-    <div class="localization-map">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3742.6851404678705!2d${data.longitude}!3d${data.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1619666668069!5m2!1spt-BR!2sbr"
-        width="350"
-        height="350"
-        style="border: 0"
-        allowfullscreen=""
-        loading="lazy">
-      </iframe>
-    </div>
+    <div><strong>Cidade:</strong><span>${data.city} / ${data.region_iso_code}</span></div>
+    <div><strong>País:</strong><span>${data.country} ${data.flag.emoji}</span></div>
+    <div><strong>Horário atual:</strong><span>${data.timezone.current_time}</span></div>
+    <div><strong>É uma VPN:</strong><span>${data.security.is_vpn ? 'Sim' : 'Não'}</span></div>
   `;
 		infoElement.innerHTML = html;
+
+		App.LoadMap(data);
+	},
+
+	LoadMap(data) {
+		const map = L.map('mapid').setView([data.latitude, data.longitude], 13);
+
+		L.tileLayer(
+			'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+			{
+				maxZoom: 18,
+				id: 'mapbox/navigation-night-v1',
+				tileSize: 512,
+				zoomOffset: -1,
+				accessToken: 'pk.eyJ1IjoiZWxpZXplcjUzNyIsImEiOiJja2c2eXU2YmswMjhmMnlvYXU2MzNrbXY1In0.akAj2BNx4Gb8fxi12W-HJQ'
+			}
+		).addTo(map);
+
+		const marker = L.marker([data.latitude, data.longitude]).addTo(map);
+		const circle = L.circle([data.latitude, data.longitude], {
+			color: '#8A8FFF',
+			fillColor: '#8976FF',
+			fillOpacity: 0.5,
+			radius: 1000,
+		}).addTo(map);
+    
+		marker.bindPopup(data.ip_address).openPopup();
+		circle.bindPopup('Margem de erro');
 	},
 };
 
